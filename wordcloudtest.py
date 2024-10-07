2a87a7bc0fa1557c89da9e3070bda00a4e9e6467
Qiuqiu0529 <1711871800@qq.com>
2024-10-07 15:31:33 +0300

    wordcloud

--- wordcloudtest.py
+++ wordcloudtest.py
@@ -2,22 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import time
+from collections import defaultdict
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv('processed_text.csv')
 label_mapping = {0: 'sadness', 1: 'joy', 2: 'love', 3: 'anger', 4: 'fear', 5: 'surprise'}
 
-start_time = time.time()
-grouped_text = df.groupby('label')
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"group text{execution_time}s")
+category_text = df.groupby('label')['text'].apply(' '.join).to_dict()
+vectorizer = TfidfVectorizer(ngram_range=(1,2),analyzer='word',token_pattern=u"(?u)\\b\\w+\\b")
+tfidf_matrix = vectorizer.fit_transform(category_text.values())
+feature_names = vectorizer.get_feature_names_out()
 
-for label, group in grouped_text:
-    text1 = ' '.join(group['text'].tolist())
-    wordcloud = WordCloud(background_color='white').generate(text1)
+filtered_category_word_freq = defaultdict(dict)
+for idx, (label, _) in enumerate(category_text.items()):
+    tfidf_scores = tfidf_matrix[idx].toarray()[0]
+    for word, score in zip(feature_names, tfidf_scores):
+        if score > 0.1:  # 示例阈值，需根据实际情况调整
+            filtered_category_word_freq[label][word] = score
 
-    plt.figure()
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.title(f'{label_mapping[label]}')
-    plt.show()
